154e0bd9bf6ac6f69c4618592a5515ac91fc1a76
Dmitry Ilyin <dima@doty.ru>
2022-08-24 16:08:40 +0300

    Add more hi2gk rules and keep them sorted

--- scripts/hi2gk.sh
+++ scripts/hi2gk.sh
@@ -8,44 +8,51 @@ do_file() {
   sed 's/\r//g' -i $1
 
   sed -i \
-    -e "s/HI_FAILURE/GK_FAILURE/g" \
-    -e "s/HI_SUCCESS/GK_SUCCESS/g" \
-    -e 's/HI_NULL/GK_NULL/g' \
-    -e 's/HI_BOOL/GK_BOOL/g' \
-    -e 's/HI_FALSE/GK_FALSE/g' \
-    -e 's/HI_TRUE/GK_TRUE/g' \
-    -e 's/HI_U8/GK_U8/g' \
-    -e 's/HI_S8/GK_S8/g' \
-    -e 's/HI_U16/GK_U16/g' \
-    -e 's/hi_u16/gk_u16/g' \
-    -e 's/HI_S32/GK_S32/g' \
-    -e 's/HI_U32/GK_U32/g' \
-    -e 's/HI_UL/GK_UL/g' \
-    -e 's/hi_u32/gk_u32/g' \
-    -e 's/HI_U64/GK_U64/g' \
-    -e 's/HI_CHAR/GK_CHAR/g' \
-    -e 's/HI_VOID/GK_VOID/g' \
-    -e 's/HI_UINTPTR_T/GK_UINTPTR_T/g' \
-    -e 's/HI_ID_/MOD_ID_/g' \
-    -e 's/HI_ERR_/ERR_CODE_/g' \
-    -e 's/__HuaweiLite__/__LITEOS__/g' \
-    -e 's/HI_PRINT/GK_PRINT/g' \
+    $1
     -e 's/CONFIG_HI_/CONFIG_/g' \
     -e 's/CONFIG_HISI_/CONFIG_/g' \
+    -e 's/HI_BOOL/GK_BOOL/g' \
+    -e 's/HI_CHAR/GK_CHAR/g' \
     -e 's/HI_DBG_/MODULE_DBG_/g' \
-    -e 's/HI_ISP_/ISP_/g' \
+    -e 's/HI_ERR_/ERR_CODE_/g' \
+    -e 's/hi_ext_/ext_/g' \
+    -e 's/HI_EXT_/ISP_EXT_/g' \
+    -e "s/HI_FAILURE/GK_FAILURE/g" \
+    -e 's/HI_FALSE/GK_FALSE/g' \
+    -e 's/HI_FLOAT/GK_FLOAT/g' \
+    -e 's/HI_ID_/MOD_ID_/g' \
     -e 's/HiISPD/ISPD/g' \
     -e 's/hi_isp_/isp_/g' \
+    -e 's/HI_ISP_/ISP_/g' \
     -e 's/hil_is_phys_in_mmz/mmz_is_phys_in_mmz/g' \
     -e 's/hil_map_mmz_check_phys/mmz_map_mmz_check_phys/g' \
-    -e 's/typedef struct hiVI_/typedef struct VI_/g' \
+    -e 's/HI_NULL/GK_NULL/g' \
+    -e 's/HI_PRINT/GK_PRINT/g' \
+    -e 's/HI_S16/GK_S16/g' \
+    -e 's/HI_S32/GK_S32/g' \
+    -e 's/HI_S64/GK_S64/g' \
+    -e 's/HI_S8/GK_S8/g' \
+    -e 's/hi_snprintf/snprintf_s/g' \
+    -e "s/HI_SUCCESS/GK_SUCCESS/g" \
+    -e 's/HI_TRUE/GK_TRUE/g' \
+    -e 's/hi_u16/gk_u16/g' \
+    -e 's/HI_U16/GK_U16/g' \
+    -e 's/hi_u32/gk_u32/g' \
+    -e 's/HI_U32/GK_U32/g' \
+    -e 's/HI_U64/GK_U64/g' \
+    -e 's/HI_U8/GK_U8/g' \
+    -e 's/HI_UINTPTR_T/GK_UINTPTR_T/g' \
+    -e 's/HI_UL/GK_UL/g' \
+    -e 's/HI_VOID/GK_VOID/g' \
+    -e 's/__HuaweiLite__/__LITEOS__/g' \
+    -e 's/#include "hi_/#include "/g' \
+    -e 's/#include "math.h"/#include "math_fun.h"/g' \
+    -e 's/typedef enum hi/typedef enum /g' \
     -e 's/typedef struct hiISP_/typedef struct ISP_/g' \
     -e 's/typedef struct hi/typedef struct /g' \
     -e 's/typedef struct HI_/typedef struct /g' \
-    -e 's/typedef enum hi/typedef enum /g' \
-    -e 's/#include "hi_/#include "/g' \
-    -e 's/#include "math.h"/#include "math_fun.h"/g' \
-    $1
+    -e 's/typedef struct hiVI_/typedef struct VI_/g' \
+    -e 's/typedef union hi/typedef union /g' \
 }
 
 if [ -d $1 ]; then
